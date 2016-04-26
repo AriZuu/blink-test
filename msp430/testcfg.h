6640e3d21dbc4e58dfc9b7210e8fec5bc63dc67a
Ari Suutari <ari@stonepile.fi>
2016-04-26 22:20:30 +0300

    Stack size was too small for msp430f2274 test.

--- msp430/testcfg.h
+++ msp430/testcfg.h
@@ -3,7 +3,7 @@
 #define IDLE_STACK_SIZE 38
 #define TEST_STACK_SIZE 50
 #else
-#define MAIN_STACK_SIZE	150
+#define MAIN_STACK_SIZE	160
 #define IDLE_STACK_SIZE 50
-#define TEST_STACK_SIZE 110
+#define TEST_STACK_SIZE 120
 #endif
